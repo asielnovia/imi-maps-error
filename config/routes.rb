cdb67df503d607fb176e965bd2a88ad392ead851
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2013-06-11 11:38:02 +0200

    add configuration for error and maintenance

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ ErrorApp::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :error
+
+  resources :maintenance
+
   # Example resource route with options:
   #   resources :products do
   #     member do
